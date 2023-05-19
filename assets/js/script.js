5fdee211b18df7d20db3697e0d22f1a85b632243
BradyLange1 <bradylange99@gmail.com>
2023-05-19 16:22:46 -0500

    configured logic for quiz loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,43 +8,70 @@ var answer3 = document.querySelector("#answer3")
 var answer4 = document.querySelector("#answer4")
 
 answers.hidden = true
+//q&a bank
+var questionBank = ["What is not a data type in JavaScript?", "What type of character surrounds an array in JavaScript?"]
+var answerBank = [
+    ["String", "Word", "Number", "Boolean"], 
+    ["Parenthesis", "Squigly Brackets", "Square Brackets", "Quotes"]
+]
+var index = 0
+var correctAnswers = ["answer2", "answer3"]
+var timeLeft = 75
+var x = true
 
-function decrementTimer(){
-    var timeLeft = 75
+//timer function
+function decrementTimer(stopTime){
     timerEl.textContent = timeLeft
-    var x = setInterval(function(){
-        if (timeLeft > 0) {
-            timeLeft--
-            timerEl.textContent = timeLeft
-        }
-    }, 1000)
-}
-
-function askQuestion(){
-    var questionBank = ["What is not a data type in JavaScript?", "What type of character surrounds an array in JavaScript?"]
-    var answerBank = [
-        ["String", "Word", "Number", "Boolean"], 
-        ["Parenthesis", "Squigly Brackets", "Square Brackets", "Quotes"]
-    ]
-    console.log(answerBank[0][0])
-    answer1.textContent = "hello"
-    for (var i = 0; i < questionBank.length; i++){
-        question.textContent = questionBank[i]
-        for (var i = 0; i < 4; i++){
-            answer1.textContent = (answerBank[i][0])
-            answer2.textContent = (answerBank[i][1])
-            answer3.textContent = (answerBank[i][2])
-            answer4.textContent = (answerBank[i][3])
+    if (timeLeft > 0){
+        if (x){
+            var time = setInterval(function(){
+                if (timeLeft > 0) {
+                    timeLeft--
+                    timerEl.textContent = timeLeft
+                }
+            }, 1000)
+            x = false
         }
     }
 }
 
+//renders question
+function askQuestion(i){
+    question.textContent = questionBank[i]
+    answer1.textContent = (answerBank[i][0])
+    answer2.textContent = (answerBank[i][1])
+    answer3.textContent = (answerBank[i][2])
+    answer4.textContent = (answerBank[i][3])
+}
 
-
+//starts quiz
 startButton.addEventListener("click", function(){
     answers.hidden = false
     startButton.remove()
     decrementTimer()
-    askQuestion()
+    askQuestion(index)
+})
+
+//user answers questions
+answers.addEventListener("click", function(event){
+    var userAnswer = event.target.id
+    if (userAnswer !== correctAnswers[index]){
+        console.log("wrong answer")
+        timeLeft -= 10
+        decrementTimer()
+    } else {
+        console.log("correct answer")
+    }
+    if (index < 1){
+        index++
+        askQuestion(index)
+    } else {7
+        if (timerEl.textContent < 0){
+            timerEl.textContent = 0
+        }
+        question.textContent = "Your score: " + timerEl.textContent
+        localStorage.setItem("score", timerEl.textContent)
+        answers.remove()
+    }
 })
 
